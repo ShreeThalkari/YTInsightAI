798ff1babd797d0d48bc9eaccc2f11b1734dd766
unknown <shreethalkari07@gmail.com>
2025-05-11 17:18:30 +0530

    Updated to handle various error in case they occur telling user about the error which occurred

--- modules/transcript_fetcher.py
+++ modules/transcript_fetcher.py
@@ -1,7 +1,11 @@
-from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api import (YouTubeTranscriptApi, YouTubeTranscriptApiException, VideoUnavailable, TranscriptsDisabled, NoTranscriptFound, CouldNotRetrieveTranscript)
 
 class Fetecher:
     def yt_transcript(self, code):
-        transcript = YouTubeTranscriptApi.get_transcript(code)
-        text = " ".join(entry['text'].replace("\n", " ").strip() for entry in transcript)
-        return text        
+        try:
+            transcript = YouTubeTranscriptApi.get_transcript(code)
+            text = " ".join(entry['text'].replace("\n", " ").strip() for entry in transcript)
+            return text        
+        
+        except (VideoUnavailable, TranscriptsDisabled, NoTranscriptFound, CouldNotRetrieveTranscript, YouTubeTranscriptApiException, Exception) as e:
+            return f"❌ Failed to get transcript: {e}"
